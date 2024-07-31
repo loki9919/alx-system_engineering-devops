d1ff00d3b847ad9a75bfb38b6264c2ddcc5846ff
lokmane rouijel <lokmane9919@gmail.com>
2024-07-31 18:24:36 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
         return 0
     
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
+    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/Feisty-Musician7531)'}
     
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
