91fb1812e5f0bc0a1e4e2c474e6628b9577c3726
lokmane rouijel <lokmane9919@gmail.com>
2024-07-31 18:50:45 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,10 +4,11 @@ Contains the number_of_subscribers function
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
-    
-    
+
+
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
