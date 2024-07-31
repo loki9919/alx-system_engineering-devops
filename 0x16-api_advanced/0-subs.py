e798459169a41dc63876cfec3185b6dff812eec5
lokmane rouijel <lokmane9919@gmail.com>
2024-07-31 18:27:56 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,19 +8,18 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/Feisty-Musician7531)'}
-    
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            return data.get("data", {}).get("subscribers", 0)
-        else:
-            return 0
+        headers = {"User-Agent": "/u/Feisty-Musician7531"}
+        with requests.get(
+            "https://www.reddit.com/r/{}/about.json".format(subreddit),
+            timeout=30,
+            headers=headers,
+            allow_redirects=False,
+        ) as r:
+            if r.status_code == 200:
+
+                return r.json().get("data").get("subscribers")
+            else:
+                return 0
     except requests.RequestException:
         return 0
