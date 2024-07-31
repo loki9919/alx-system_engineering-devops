77566f07dc1036c7289857ec91fff52721f31b9b
lokmane rouijel <lokmane9919@gmail.com>
2024-07-31 18:26:09 +0100

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ def top_ten(subreddit):
     """
         returns the top ten posts for a given subreddit
     """
-    user = {'User-Agent': 'Lizzie'}
+    user = {'User-Agent': 'Feisty-Musician7531'}
     url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
                        .format(subreddit), headers=user).json()
     try:
